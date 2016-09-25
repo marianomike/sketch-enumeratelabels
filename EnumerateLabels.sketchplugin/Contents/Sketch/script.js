c0d9627cf0fd43a76c15c42b6563e26d9dca3370
Mike Mariano <mike@uiuxartist.com>
2016-09-24 21:27:52 -0700

    Add comments to code

--- EnumerateLabels.sketchplugin/Contents/Sketch/script.js
+++ EnumerateLabels.sketchplugin/Contents/Sketch/script.js
@@ -1,25 +1,41 @@
 var onRun = function(context) {
 
+  //reference the document
   var doc = context.document;
+
+  //reference the selected layers
   var selectedLayers = context.selection;
+
+  //count the selected layers
   var selectedCount = selectedLayers.count();
 
   if (selectedCount == 0) {
+    //show a message if nothing is selected
     doc.showMessage("No layers are selected.");
   } else {
-    log('Selected layers:');
+    //create an array to hold all the text layers if other types of layers are selected
     var textLayerArray = [];
 
+    //loop through the selected layers
     for (var i = 0; i < selectedCount; i++) {
+
+      //reference each layer
       var layer = selectedLayers[i];
+
+      //check if the layer is a text layer
       if(layer.class() == "MSTextLayer"){
+        //if it is push it into the text array
         textLayerArray.push(layer);
       }
     }
 
+    //loop through the text array
     for (var z = 0; z < textLayerArray.length; z++){
+      //reference each text layer
       var textLayer = textLayerArray[z];
+      //create a string out of the array number + 1 since arrays start at 0
       var textString = String(z + 1);
+      //set the string value to the array number
       textLayer.setStringValue(textString);
     }
   };
